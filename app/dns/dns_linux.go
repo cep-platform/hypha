7b5997aa5510e271b8096b1bb8bdbce97b6bd3cb
AdamWewe <adammehdiarafan@gmail.com>
2026-03-31 00:57:23 +0200

    todos for l8r

--- app/dns/dns_linux.go
+++ app/dns/dns_linux.go
@@ -3,6 +3,10 @@ package dns
 import (
 	"net"
   "github.com/godbus/dbus/v5"
+	"bytes"
+	"log"
+	"os/exec"
+	"fmt"
 )
 
 func GetGlobalDNS() ([]string, error) {
@@ -38,7 +42,34 @@ func GetGlobalDNS() ([]string, error) {
 		return append(dnsArrTemp, ip), nil
 }
 
-func SetGlobalDNS([]string) error {
+//TODO: Tomorrow: check if NMCLI is installed
+//Do greps:
+//	- get which one is active
+//	- nmcli connection show --active | grep ethernet/Wi-fi
+//	- sudo nmcli connection modify "ethernet" ipv4.dns "8.8.8.8 8.8.4.4"
+//  - That should be it
+func SetGlobalDNS(servers []string) error {
+	
+	for _, server := range servers {
+			
+		cmd := exec.Command("networksetup", "-setdnsservers", "Wi-Fi", server)
+
+		//buffers for collecting output
+		var stdoutBuff bytes.Buffer
+		var stderrBuff bytes.Buffer
+
+		cmd.Stdout = &stdoutBuff
+		cmd.Stderr = &stderrBuff
+
+	
+		if err:= cmd.Run(); err != nil {
+			stderrString := stderrBuff.String()
+			log.Printf("Error setting DNS address %s", stderrString)
+			return fmt.Errorf("error setting DNS address %s", stderrString)
+		}
+		
+		log.Printf("successfully set DNS server %s", server)
+	}
 	return nil
 }
 
